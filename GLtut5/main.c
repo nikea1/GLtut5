10df3f2745896c201b8ccdebdc324a5661b8b616
Adenike Adeyemi <nike.adeyemi1@gmail.com>
2018-11-21 01:06:13 -0500

    Initial Commit

---
+++ GLtut5/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  GLtut5
+//
+//  Created by Adenike Adeyemi on 11/21/18.
+//  Copyright © 2018 Adenike Adeyemi. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
