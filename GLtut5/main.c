aac4e50330de08ded769ca432e156cb10f9fcbdd
Adenike Adeyemi <nike.adeyemi1@gmail.com>
2018-11-22 02:37:38 -0500

    Rendered a black square

--- GLtut5/main.c
+++ GLtut5/main.c
@@ -27,13 +27,17 @@
 int main(int argc, const char * argv[]) {
     GLFWwindow *window;
     
+    //-----------------------------------------------------
+    //Initialize GLFW and GLAD
+    //-----------------------------------------------------
+    
     //Initialize GLFW window
     if(!glfwInit()){
         printf("GLFW could not initialize.\n");
         exit(EXIT_FAILURE);
     }
     
-    //GLFW for apple
+    //GLFW hin for apple
 #ifdef __APPLE__
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
@@ -56,17 +60,75 @@ int main(int argc, const char * argv[]) {
         printf("GLAD failed to load.\n");
         exit(EXIT_FAILURE);
     }
+    //------------------------------------------------------
+    //OpenGL Code Starts Here
+    //------------------------------------------------------
     
+    GLuint program = initShaderFiles("vshader.vert", "fshader.frag");
+    GLuint vao; //Vertex Array Object
+    GLuint vbo; //Vertex Buffer Object
+    GLuint ebo; //Element Buffer Object
+    
+    GLfloat vertices[] = {
+        //Position
+        0.5f,  0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+       -0.5f, -0.5f, 0.0f,
+       -0.5f,  0.5f, 0.0f
+    };
+    
+    GLuint elements[]={
+      0, 1, 2,
+      0, 2, 3
+    };
+    glUseProgram(program);
+    
+    //------------------------------------------------------
+    //Buffers
+    //------------------------------------------------------
+    //Vertex Array
+    glGenVertexArrays(1, &vao);                                                        //Generate Vertex Array that will hold the array buffers
+    glBindVertexArray(vao);                                                            //Bind Vertex Array
+    
+    //Array Buffer
+    glGenBuffers(1, &vbo);                                                             //gernerate array buffer
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);                                                //bind Array buffer
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);         //set data in array buffer to draw
+    
+    //Element Buffer
+    glGenBuffers(1, &ebo);                                                             //generate element buffer
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);                                        //bind element array buffer
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(elements), elements, GL_STATIC_DRAW); //set data in element array buffer to draw
+    
+    
+    //------------------------------------------------------
+    //Set Attributes
+    //------------------------------------------------------
+    //Position attributes in layout/index 0
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0*sizeof(GL_FLOAT), (GLvoid*)0);   //define position attribute data
+    glEnableVertexAttribArray(0);                                                      //Activate layout 0
+    
+    //------------------------------------------------------
+    //Loop Starts Here
+    //------------------------------------------------------
     while(!glfwWindowShouldClose(window)){
         
         //Clear Window screen
-        glClearColor(1.0, 1.0, 1.0, 1.0);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(1.0, 1.0, 1.0, 1.0);                                              //Set a clear color to white
+        glClear(GL_COLOR_BUFFER_BIT);                                                  //Clear color with the clear color
+        
+        //Draw
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);                           //Reads Element Buffer and draws
         
         //swap frames and IO
-        glfwSwapBuffers(window);
-        glfwPollEvents();
+        glfwSwapBuffers(window);                                                       //swap frames
+        glfwPollEvents();                                                              //Check for IO
     }
+    //-------------------------------------------------------
+    //End
+    //-------------------------------------------------------
     printf("Hello, World!\n");
     return 0;
 }
+
+
---
+++ vshader.vert
@@ -0,0 +1,16 @@
+#version 330
+layout(location=0) in vec3 aPos;
+//layout(location=1) in vec3 aColor;
+//layout(location=2) in vec2 aTex;
+
+//uniform mat4 model;
+//uniform mat4 view;
+//uniform mat4 projection;
+
+//out vec2 aTexCoord;
+out vec3 myColor;
+
+void main(){
+    gl_Position = vec4(aPos, 1.0);
+    //myColor = aColor;
+}
